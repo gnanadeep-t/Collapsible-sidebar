076e19e13c56b19706e8551c7da3da097d88851a
gnanadeep-t <gnanadeep.t@gmail.com>
2024-04-01 22:52:26 +0530

    completed collapsible navbar as required

--- src/components/OurComps/NavMenuDemo.js
+++ src/components/OurComps/NavMenuDemo.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import {
   ChevronRightIcon,
@@ -6,6 +6,7 @@ import {
   HomeIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
+import { Separator } from "../ui/separator";
 import Link from "next/link";
 
 export default function Sidebar() {
@@ -14,36 +15,38 @@ export default function Sidebar() {
 
   const navigation = [
     { name: "Dashboard", href: "/", icon: HomeIcon },
-    { name: "Dev", href: "/dev", icon: UserCircleIcon },
+    { name: "Dev", icon: UserCircleIcon },
     { name: "Purchase", href: "/purchase", icon: HomeIcon },
-    { name: "Production", href: "/production", icon: HomeIcon },
+    { name: "Production", icon: HomeIcon },
   ];
 
+  const handleMenuItemClick = (item) => {
+    if (item.submenus) {
+      setOpenSubMenu(openSubMenu === item.name ? null : item.name);
+    } else if (item.href) {
+      // Navigate to the specified href
+      window.location.href = item.href;
+    }
+  };
 
   return (
     <div
-      className={` min-h-screen bg-customGrayBackground p-7 border-r-2 z-30 border-white-100 transition-all duration-300 ${
+      className={`h-screen bg-customGrayBackground p-7 border-r-2 z-30 border-white-100 transition-all duration-300 ${
         isHovered ? "w-48" : "w-16"
       }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {/* Logo */}
-      <div className="text-2xl text-primary m-1 p-1 pb-2">Logo</div>
+      {/* <div className="text-2xl text-primary m-1 p-1 pb-2">Logo</div> */}
       {/* Menu Items */}
-      <ul>
+      <ul className="">
         {navigation.map((item, index) => (
           <li key={index}>
-            <Link href={item.href}>
-              <div
-                className={`flex items-center px-3 py-2 rounded-md text-customFont font-medium hover:text-gray-600 w-full text-left cursor-pointer
-                ${isHovered} ? "opacity-100" : "opacity-0"
-                `}
-              >
-                <item.icon className="w-5 h-5 mr-2 text-customFont opacity-100" />
-                <span>{item.name}</span>
-              </div>
-            </Link>
+            <div className="inline-flex">
+                <item.icon className="w-5 h-5 text-customFont m-3.5 ml-0 " />
+                <div className={`h-5 m-3.5 ml-0 transition-all duration-200  ${isHovered?"":"opacity-0 "}`}>{item.name}</div>
+            </div>
           </li>
         ))}
       </ul>
